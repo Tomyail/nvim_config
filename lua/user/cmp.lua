817adaddff5449954e6f44b8ac25fa583e4e938a
lixuexin03 <lixuexin03@meituan.com>
2023-03-31 17:47:04 +0800

    change(cmp): using j k instead of tab s-tab for select item because it conflict with coplit

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -19,6 +19,7 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
+-- 目的是检查光标是否在行的开始位置或者位于一个空白字符之后
 local check_backspace = function()
 	local col = vim.fn.col(".") - 1
 	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
@@ -102,28 +103,28 @@ cmp.setup({
 			-- behavior = cmp.ConfirmBehavior.Replace,
 			select = false,
 		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expandable() then
-				luasnip.expand()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif check_backspace() then
-				fallback()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+		--[[ ["<Tab>"] = cmp.mapping(function(fallback) ]]
+		--[[ 	if cmp.visible() then ]]
+		--[[ 		cmp.select_next_item() ]]
+		--[[ 	elseif luasnip.expandable() then ]]
+		--[[ 		luasnip.expand() ]]
+		--[[ 	elseif luasnip.expand_or_jumpable() then ]]
+		--[[ 		luasnip.expand_or_jump() ]]
+		--[[ 	elseif check_backspace() then ]]
+		--[[ 		fallback() ]]
+		--[[ 	else ]]
+		--[[ 		fallback() ]]
+		--[[ 	end ]]
+		--[[ end, { "i", "s" }), ]]
+		--[[ ["<S-Tab>"] = cmp.mapping(function(fallback) ]]
+		--[[ 	if cmp.visible() then ]]
+		--[[ 		cmp.select_prev_item() ]]
+		--[[ 	elseif luasnip.jumpable(-1) then ]]
+		--[[ 		luasnip.jump(-1) ]]
+		--[[ 	else ]]
+		--[[ 		fallback() ]]
+		--[[ 	end ]]
+		--[[ end, { "i", "s" }), ]]
 	}),
 	formatting = {
 		--[[ fields = { "kind", "abbr", "menu" }, ]]
