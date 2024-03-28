61988e89035a3b5f99b0b519c34886aeb79eb790
Leo sun <tomyail1989@gmail.com>
2024-03-29 00:02:22 +0800

    Add nvim-notify plugin and update dependencies

--- smart-im/lua/init.lua
+++ smart-im/lua/init.lua
@@ -11,7 +11,7 @@ function M.check_and_switch_input_method()
 
 	local function is_chinese(char)
 		local utf8_value = vim.fn.char2nr(char)
-		print("char: " .. char .. "utf8_value: " .. utf8_value)
+		-- print("char: " .. char .. "utf8_value: " .. utf8_value)
 		return utf8_value >= 0x4E00 and utf8_value <= 0x9FFF
 	end
 
@@ -20,7 +20,7 @@ function M.check_and_switch_input_method()
 	local line_to_cursor = line:sub(1, col)
 	local char_col = vim.fn.strchars(line_to_cursor)
 	local current_char = vim.fn.matchstr(line, ".", char_col)
-	print("current_char: " .. current_char .. "char_col: " .. char_col)
+	-- print("current_char: " .. current_char .. "char_col: " .. char_col)
 	local prev_char = char_col > prev_offset and vim.fn.matchstr(line, ".", char_col - prev_offset) or ""
 	local next_char = char_col < vim.fn.strchars(line) and vim.fn.matchstr(line, ".", char_col + 1) or ""
 	--[[ local current_char = vim.fn.matchstr(line, ".", col) ]]
@@ -33,6 +33,7 @@ function M.check_and_switch_input_method()
 		M.switch_to_english()
 	end
 end
+
 -- 辅助函数：去除字符串首尾的空白字符
 local function trim(s)
 	return s:match("^%s*(.-)%s*$")
@@ -67,7 +68,7 @@ function M.switch_to_chinese()
 end
 
 function M.setup(config)
-	print("smart-im setup")
+	-- print("smart-im setup")
 	local defaultConfig = {
 		logLevel = "info",
 		lang = {
@@ -110,10 +111,6 @@ function M.setup(config)
 		return nil
 	end
 
-	local function onInsertEnter()
-		local x = get_character_at_cursor()
-		print("x", x)
-	end
 
 	vim.api.nvim_create_autocmd("InsertLeave", {
 		callback = function()
@@ -123,45 +120,96 @@ function M.setup(config)
 
 	vim.api.nvim_create_autocmd("InsertEnter", {
 		callback = function()
-			-- function get_chars_of_current_line()
-			-- 	local current_line = vim.api.nvim_get_current_line()
-			-- 	local chars = {}
-			-- 	for i = 1, #current_line do
-			-- 		local char = current_line:sub(i, i)
-			-- 		table.insert(chars, char)
-			-- 	end
-			-- 	return chars
-			-- end
+			local function get_utf8_char(str, col)
+				-- lua's array index starts from 1!!jk
+				local pre_p = 1
+				local pre_c = ""
+				local ranges = {}
 
+				for p, c in utf8.codes(str) do
+					local range = { start_idx = pre_p, end_idx = p, char = pre_c }
+					table.insert(ranges, range)
+					pre_p = p
+					pre_c = c
+				end
+
+				
+				-- https://www.lddgo.net/en/string/cjk-unicode
+				local chinese_ranges = {
+					{ 0x4E00, 0x9FFF },
+					{ 0x3400, 0x4DBF },
+					{ 0x20000, 0x2A6DF },
+					{ 0x2A700, 0x2B73F },
+					{ 0x2B740, 0x2B81F },
+					{ 0x2B820, 0x2CEAF },
+					{ 0x2CEB0, 0x2EBEF },
+					{ 0x30000, 0x3134A },
+					{ 0x31350, 0x323AF}
+				}
+
+				local is_code_point_in_ranges = function(code_point, ranges)
+					for _, range in ipairs(ranges) do
+						if code_point >= range[1] and code_point <= range[2] then
+							return true
+						end
+					end
+					return false
+				end
+
+				-- targe char is the char at col > start_index and col <= end_index
+
+				for _, range in ipairs(ranges) do
+					if col >= range.start_idx and col < range.end_idx then
+						vim.notify(
+							"range: "
+								.. vim.inspect(range)
+								.. " col:"
+								.. col
+								.. " codepoint:"
+								.. "0x"..string.format("%x",utf8.codepoint(range.char))
+								.. "is_chinese:".. tostring(is_code_point_in_ranges(utf8.codepoint(range.char), chinese_ranges))
+						)
+
+						if(is_code_point_in_ranges(utf8.codepoint(range.char), chinese_ranges)) then
+							M.switch_to_chinese()
+						else
+							M.switch_to_english()
+						end
+
+
+					end
+				end
+				return ""
+			end
 			local function get_cursor_column()
 				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 				print("line: " .. line)
 				print("col: " .. col)
 				return col
-			  end
+			end
 
-			function get_chars_of_current_line()
-
-
+			local function get_chars_of_current_line()
 				get_cursor_column()
 				local current_line = vim.api.nvim_get_current_line()
 				-- local chars = {}
 				-- local i = 1
-				print("current_line: " .. current_line)
+
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				local result = get_utf8_char(current_line, col)
 				-- local len = utf8.len(current_line)
 				-- print("len: " .. len)
 				-- local codes = utf8.codes(current_line)
 				-- print("codes: " .. vim.inspect(codes))
 
-				for p, c in utf8.codes(current_line) do 
-					print("p: " .. p)
-					print("c: " .. c)
-				end
+				-- for p, c in utf8.codes(current_line) do
+				-- 	print("p: " .. p)
+				-- 	print("c: " .. c)
+				-- end
 				-- while i <= len do
 				-- 	print("i".. i)
 				-- 	local codepoint = utf8.codepoint(current_line, i)
 				-- 	local offset = utf8.offset(current_line, i)
-					
+
 				-- 	print("codepoint: " .. codepoint)
 				-- 	print("offset: " .. offset)
 				-- 	local offsetChat = utf8.char(offset)
@@ -179,8 +227,10 @@ function M.setup(config)
 				-- end
 				return chars
 			end
-			print(vim.inspect(get_chars_of_current_line()))
-			M.check_and_switch_input_method()
+
+			get_chars_of_current_line()
+			-- print(vim.inspect(get_chars_of_current_line()))
+			-- M.check_and_switch_input_method()
 		end,
 	})
 end
