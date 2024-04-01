4381e82a99e02672d77b7210cc3d7c541e0ef1ab
lixuexin03 <lixuexin03@meituan.com>
2024-04-01 17:49:17 +0800

    style(smart-im): 调整代码格式以符合规范

--- smart-im/lua/smart-im/init.lua
+++ smart-im/lua/smart-im/init.lua
@@ -5,124 +5,124 @@ local logger = require("smart-im.logger")
 logger.set_using_notify(true)
 
 function M.get_matched_lang(lang_config, codepoint)
-	local is_code_point_in_ranges = function(code_point, ranges)
-		for _, range in ipairs(ranges) do
-			if code_point >= range[1] and code_point <= range[2] then
-				return true
-			end
-		end
-		return false
-	end
-	for _, lang in pairs(lang_config) do
-		if is_code_point_in_ranges(codepoint, lang.ranges) then
-			return lang
-		end
-	end
+    local is_code_point_in_ranges = function(code_point, ranges)
+        for _, range in ipairs(ranges) do
+            if code_point >= range[1] and code_point <= range[2] then
+                return true
+            end
+        end
+        return false
+    end
+    for _, lang in pairs(lang_config) do
+        if is_code_point_in_ranges(codepoint, lang.ranges) then
+            return lang
+        end
+    end
 end
 
 function M.find_in_array(table, fn)
-	for _, v in ipairs(table) do
-		if fn(v) then
-			return v
-		end
-	end
+    for _, v in ipairs(table) do
+        if fn(v) then
+            return v
+        end
+    end
 end
 function M.change_input_by_os(lang)
-	local methods = lang.methods
-	local lang_name = lang.name
-	local os_name = M.os_name()
-	local target = M.find_in_array(methods, function(method)
-		return method.os == os_name
-	end)
-	if target then
-		if vim.fn.executable(target.cmd) == 1 then
-			local command = string.format("%s %s", target.cmd, target.input)
-			local status, exit_type, _ = os.execute(command)
-			if status == 0 then
-				local msg =
-					string.format("run command %s for lang: %s on os: %s successfully", command, lang_name, os_name)
-				logger.debug(msg)
-			else
-				local msg = string.format("run command %s for lang: %s on os: %s failed", command, lang_name, os_name)
-				logger.warn(msg)
-			end
-		else
-			local msg = string.format(
-				"Command %s not found in lang: %s for os : %s. please ensure command exist",
-				target.cmd,
-				lang_name,
-				os_name
-			)
-			logger.warn(msg)
-		end
-	else
-		local msg = string.format(
-			"No matched input method find in lang: %s for os: %s. please check your config",
-			lang_name,
-			os_name
-		)
-		logger.warn(msg)
-	end
+    local methods = lang.methods
+    local lang_name = lang.name
+    local os_name = M.os_name()
+    local target = M.find_in_array(methods, function(method)
+        return method.os == os_name
+    end)
+    if target then
+        if vim.fn.executable(target.cmd) == 1 then
+            local command = string.format("%s %s", target.cmd, target.input)
+            local status, exit_type, _ = os.execute(command)
+            if status == 0 then
+                local msg =
+                    string.format("run command %s for lang: %s on os: %s successfully", command, lang_name, os_name)
+                logger.debug(msg)
+            else
+                local msg = string.format("run command %s for lang: %s on os: %s failed", command, lang_name, os_name)
+                logger.warn(msg)
+            end
+        else
+            local msg = string.format(
+                "Command %s not found in lang: %s for os : %s. please ensure command exist",
+                target.cmd,
+                lang_name,
+                os_name
+            )
+            logger.warn(msg)
+        end
+    else
+        local msg = string.format(
+            "No matched input method find in lang: %s for os: %s. please check your config",
+            lang_name,
+            os_name
+        )
+        logger.warn(msg)
+    end
 end
 
 function M.get_range(str, col)
-	-- lua's array index starts from 1!!
-	local pre_p = 0
-	local pre_c = ""
-	local ranges = {}
+    -- lua's array index starts from 1!!
+    local pre_p = 0
+    local pre_c = ""
+    local ranges = {}
 
-	for p, c in utf8.codes(str) do
-		local range = { start_idx = pre_p, end_idx = p, char = pre_c }
-		table.insert(ranges, range)
-		pre_p = p
-		pre_c = c
-	end
-	table.insert(ranges, { start_idx = pre_p, end_idx = string.len(str), char = pre_c })
-	table.remove(ranges, 1)
+    for p, c in utf8.codes(str) do
+        local range = { start_idx = pre_p, end_idx = p, char = pre_c }
+        table.insert(ranges, range)
+        pre_p = p
+        pre_c = c
+    end
+    table.insert(ranges, { start_idx = pre_p, end_idx = string.len(str), char = pre_c })
+    table.remove(ranges, 1)
 
-	local normal_col = col
-	for _, range in ipairs(ranges) do
-		if normal_col >= range.start_idx and normal_col <= range.end_idx then
-			return range
-		end
-	end
+    local normal_col = col
+    for _, range in ipairs(ranges) do
+        if normal_col >= range.start_idx and normal_col <= range.end_idx then
+            return range
+        end
+    end
 end
 
 function M.os_name()
-	return string.lower(vim.loop.os_uname().sysname)
+    return string.lower(vim.loop.os_uname().sysname)
 end
 
 function M.setup(config)
-	local defaultConfig = require("smart-im.config")
-	local merged_config = vim.tbl_deep_extend("force", defaultConfig, config)
-	-- TODO validate config
+    local defaultConfig = require("smart-im.config")
+    local merged_config = vim.tbl_deep_extend("force", defaultConfig, config)
+    -- TODO validate config
 
-	logger.set_log_level(merged_config.logLevel)
-	vim.api.nvim_create_autocmd("InsertLeave", {
-		callback = function()
-			M.change_input_by_os(merged_config.default)
-		end,
-	})
+    logger.set_log_level(merged_config.logLevel)
+    vim.api.nvim_create_autocmd("InsertLeave", {
+        callback = function()
+            M.change_input_by_os(merged_config.default)
+        end,
+    })
 
-	vim.api.nvim_create_autocmd("InsertEnter", {
-		callback = function()
-			local current_line_content = vim.api.nvim_get_current_line()
-			local _line_idx, col_idx = unpack(vim.api.nvim_win_get_cursor(0))
-			local range = M.get_range(current_line_content, col_idx)
-			if range then
-				local codepoint = utf8.codepoint(range.char)
-				local msg = string.format("col_idx: %s, char: %s,  codepoint: %x", col_idx, range.char, codepoint)
-				logger.debug(msg)
-				local lang = M.get_matched_lang(merged_config.lang, codepoint)
+    vim.api.nvim_create_autocmd("InsertEnter", {
+        callback = function()
+            local current_line_content = vim.api.nvim_get_current_line()
+            local _line_idx, col_idx = unpack(vim.api.nvim_win_get_cursor(0))
+            local range = M.get_range(current_line_content, col_idx)
+            if range then
+                local codepoint = utf8.codepoint(range.char)
+                local msg = string.format("col_idx: %s, char: %s,  codepoint: %x", col_idx, range.char, codepoint)
+                logger.debug(msg)
+                local lang = M.get_matched_lang(merged_config.lang, codepoint)
 
-				if lang then
-					M.change_input_by_os(lang)
-				else
-					M.change_input_by_os(merged_config.default)
-				end
-			end
-		end,
-	})
+                if lang then
+                    M.change_input_by_os(lang)
+                else
+                    M.change_input_by_os(merged_config.default)
+                end
+            end
+        end,
+    })
 end
 
 return M
--- smart-im/lua/smart-im/logger.lua
+++ smart-im/lua/smart-im/logger.lua
@@ -4,50 +4,50 @@ local lovel_list = { "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "OFF" }
 local using_notify = false
 
 M.set_using_notify = function(value)
-	using_notify = value
+    using_notify = value
 end
 M.set_log_level = function(level)
-	if vim.tbl_contains(lovel_list, level) then
-		log_level = level
-	end
+    if vim.tbl_contains(lovel_list, level) then
+        log_level = level
+    end
 end
 
 M.tbl_indexof = function(tbl, obj)
-	for i, v in ipairs(tbl) do
-		if v == obj then
-			return i
-		end
-	end
-	return -1
+    for i, v in ipairs(tbl) do
+        if v == obj then
+            return i
+        end
+    end
+    return -1
 end
 M.is_level_enabled = function(level)
-	return M.tbl_indexof(lovel_list, level) >= M.tbl_indexof(lovel_list, log_level)
+    return M.tbl_indexof(lovel_list, level) >= M.tbl_indexof(lovel_list, log_level)
 end
 M.print_msg = function(level, msg)
-	if M.is_level_enabled(level) then
-		local output = "[" .. level .. "] " .. msg
-		if using_notify then
-			vim.notify(output)
-			-- vim.notify(output, M.tbl_indexof(lovel_list, level) - 1)
-		else
-			print(output)
-		end
-	end
+    if M.is_level_enabled(level) then
+        local output = "[" .. level .. "] " .. msg
+        if using_notify then
+            vim.notify(output)
+        -- vim.notify(output, M.tbl_indexof(lovel_list, level) - 1)
+        else
+            print(output)
+        end
+    end
 end
 M.debug = function(msg)
-	M.print_msg("DEBUG", msg)
+    M.print_msg("DEBUG", msg)
 end
 
 M.info = function(msg)
-	M.print_msg("INFO", msg)
+    M.print_msg("INFO", msg)
 end
 
 M.warn = function(msg)
-	M.print_msg("WARN", msg)
+    M.print_msg("WARN", msg)
 end
 
 M.error = function(msg)
-	M.print_msg("ERROR", msg)
+    M.print_msg("ERROR", msg)
 end
 
 return M
