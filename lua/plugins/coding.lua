e5850d97085ee7b9f472c71b18fc9e42d973917d
Leo sun <tomyail1989@gmail.com>
2024-04-07 23:02:33 +0800

    enable csi u

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,52 +1,32 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    config = function(_, opts)
+    opts = function(_, opts)
       local cmp = require("cmp")
-      local merged_opts = vim.tbl_deep_extend("force", {
+      local custom = {
+
         mapping = cmp.mapping.preset.insert({
           ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
           ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
-          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-          ["<C-x>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-          ["<ESC>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping({
-            -- 在插入模式, 如果当前没有选择, 按下回车后是回车,如果当前有选择, 按下回车后是确认选择
-            i = function(fallback)
-              if cmp.visible() and cmp.get_active_entry() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-              else
-                fallback()
-              end
-            end,
-            -- 在选择模式, 按下回车后是确认选择
-            s = cmp.mapping.confirm({ select = true }),
-            -- 在命令模式, 按下回车后是确认选择, 并且会替换
-            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-          }),
+          -- ["<CR>"] = cmp.mapping({
+          --   -- 在插入模式, 如果当前没有选择, 按下回车后是回车,如果当前有选择, 按下回车后是确认选择
+          --   i = function(fallback)
+          --     if cmp.visible() and cmp.get_active_entry() then
+          --       cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+          --     else
+          --       fallback()
+          --     end
+          --   end,
+          --   -- 在选择模式, 按下回车后是确认选择
+          --   s = cmp.mapping.confirm({ select = true }),
+          --   -- 在命令模式, 按下回车后是确认选择, 并且会替换
+          --   c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+          -- }),
         }),
-      }, opts)
-      cmp.setup(merged_opts)
+      }
 
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      -- cmp.setup.cmdline({ "/", "?" }, {
-      --   mapping = cmp.mapping.preset.cmdline(),
-      --   sources = {
-      --     { name = "buffer" },
-      --   },
-      -- })
-
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      -- cmp.setup.cmdline(":", {
-      --   mapping = cmp.mapping.preset.cmdline(),
-      --   sources = cmp.config.sources({
-      --     { name = "path" },
-      --   }, {
-      --     { name = "cmdline" },
-      --   }),
-      --   matching = { disallow_symbol_nonprefix_matching = false },
-      -- })
+      local merged = vim.tbl_deep_extend("force", opts, custom)
+      return merged
     end,
   },
 
