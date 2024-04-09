5438d307481b4cea45700d68b4d3db0e300c9fe3
Leo sun <tomyail1989@gmail.com>
2024-04-09 10:51:27 +0800

    clean code

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- require("user.keymaps")
-require("user.plugins")
+-- require("user.plugins")
 -- require("user.autocommands")
 -- require("user.options")
 -- require("user.functions")
@@ -16,3 +16,30 @@ require("user.plugins")
 -- require("user.snippet")
 -- require("user.leap")
 -- require("user.mini")
+--
+--
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {
+  defaults = {
+    lazy = false, -- should plugins be lazy-loaded?
+  },
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "plugins" },
+  },
+}
+require("lazy").setup(opts)
