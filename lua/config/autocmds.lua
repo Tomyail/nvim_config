ea1e825ceefa5695aa4dedd4cf9de2f8b6a53f94
Leo sun <tomyail1989@gmail.com>
2024-04-09 10:35:01 +0800

    feat(autocmds): add spelllang setting for markdown files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+
+-- Set spelllang to en,cjk for markdown files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.spelllang = "en,cjk"
+  end,
+})
+
