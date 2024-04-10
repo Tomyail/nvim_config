4cb7288dad313d7acd9418170b2be36a2e6d2602
Leo sun <tomyail1989@gmail.com>
2024-04-10 11:46:03 +0800

    修复obsidian.nvim 创建中文标题变为空白

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -158,7 +158,9 @@ return {
         local suffix = ""
         if title ~= nil then
           -- If title is given, transform it into valid file name.
-          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+          suffix = title:gsub(" ", "-"):lower()
+          -- 会把中文移除了
+          -- :gsub("[^A-Za-z0-9-]", ""):
         else
           -- If title is nil, just add 4 random uppercase letters to the suffix.
           for _ = 1, 4 do
