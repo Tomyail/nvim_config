090c53aa7dbafda56ecf01d5cb35b16f3f274f6f
Leo sun <tomyail1989@gmail.com>
2024-04-15 18:03:21 +0800

    clean code

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,10 +5,10 @@ return {
       local function get_servers()
         local servers = {}
 
-        local files = vim.api.nvim_get_runtime_file("lua/user/lsp/settings/*", true)
+        local files = vim.api.nvim_get_runtime_file("lua/lsp/*", true)
         for _, filepath in ipairs(files) do
           local name = filepath:match(".+/([^/]+)%..+$") -- 提取文件名并去掉扩展名
-          servers[name] = require("user.lsp.settings." .. name) -- 加载文件内容
+          servers[name] = require("lsp." .. name) -- 加载文件内容
         end
 
         -- vim.notify("Loaded servers count " .. vim.tbl_count(servers))
