b8f969326a52b16f27031cdba3902fd2f9a497b9
Leo sun <tomyail1989@gmail.com>
2024-12-06 17:38:49 +0800

    add multicursors

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,9 +6,3 @@
 local map = vim.keymap.set
 local del = vim.keymap.del
 
--- 因为在whichkkey 自定义 <leader>w是保存文件,所以需要删掉 所有的子命令,否则 leader w 冲突
--- del("n", "<leader>wd")
--- del("n", "<leader>w-")
--- del("n", "<leader>w|")
--- del("n", "<leader>ww")
--- map("n", "<leader>ww", "<cmd>w!<cr>", { desc = "Save" })
