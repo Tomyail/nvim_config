12d8f13fbb9f19de22908d2b5cfd9a8aada08b52
Leo sun <tomyail1989@gmail.com>
2024-12-09 14:57:18 +0800

    support custom nodejs

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -45,22 +45,27 @@ return {
     end,
   },
 
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   event = "InsertEnter",
-  --   config = function()
-  --     require("copilot").setup({
-  --       suggestion = { enabled = false },
-  --       panel = { enabled = false },
-  --       filetypes = {
-  --         markdown = true,
-  --         yaml = true,
-  --       },
-  --       -- copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/18.19.0/bin/node", -- Node.js version must be > 18.x
-  --     })
-  --   end,
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      local node_path = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/18.19.0/bin/node"
+      local copilot_node_command = "node"
+      if vim.fn.filereadable(node_path) == 1 then
+        copilot_node_command = node_path -- Node.js version must be > 18.x
+      end
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+        filetypes = {
+          markdown = true,
+          yaml = true,
+        },
+        copilot_node_command = copilot_node_command,
+      })
+    end,
+  },
   -- {
   --   "CopilotC-Nvim/CopilotChat.nvim",
   --   -- build = "make tiktoken",
